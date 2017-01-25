347d11ba204f1f2430556f4d0fd68d354fe4c877
Evan Samona <evansamona@Macintosh.local>
2017-01-25 16:54:45 -0500

    commit implementation

--- BeerSong.java
+++ BeerSong.java
@@ -1,6 +1,22 @@
+package beersong;
+
 public class BeerSong {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
+        Ninety_Nine_Bottles_of_Beer();
     }
+    
     public static void Ninety_Nine_Bottles_of_Beer(){
+        for(int i=99;i>0;i--){
+            System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer");
+            System.out.print("take one down, pass it around, ");
+            if(i!=1){
+                System.out.println((i-1)+" bottles of beer on the wall.");
+            }
+            else{
+                System.out.println(", no more bottles of beer on the wall.");
+            }
+        }
     }
+    
 }
+
